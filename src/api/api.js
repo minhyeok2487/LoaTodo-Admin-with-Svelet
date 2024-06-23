cdbb5828b1db69567b496dc11d297ceec6b6c8a6
Minhyeok1995 <repeater2487@naver.com>
2024-06-23 23:17:20 +0900

    개발 서버 api 연동

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,5 @@
-export const API_BASE_URL = 'http://localhost:8080';
+// export const API_BASE_URL = 'http://localhost:8080';
+export const API_BASE_URL = "https://api2.loatodo.com";
 export const BASE_URL = "https://api2.loatodo.com";
 
 function getAuthHeaders() {
